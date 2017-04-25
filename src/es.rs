ba2bb7a05798362cb3e25c34943482a317ab4e8d
hdlj <hubert.delajonquiere@snips.net>
2017-04-25 18:56:25 +0200

    few fixes in the spanish number grammar

--- src/es.rs
+++ src/es.rs
@@ -9,7 +9,7 @@ pub fn rules_numbers() -> RustlingResult<RuleSet<Dimension>> {
     Ok(RuleSet(vec![
         rule! {
             "number (0..15)",
-            (regex!(r#"((c|z)ero|un(o|a)?|dos|tr(é|e)s|cuatro|cinco|s(e|é)is|siete|ocho|nueve|die(z|s)|once|doce|trece|catorce|quince)"#)),
+            (regex!(r#"((?:c|z)ero|un(?:o|a)?|dos|tr(?:é|e)s|cuatro|cinco|s(?:e|é)is|siete|ocho|nueve|die(?:z|s)|once|doce|trece|catorce|quince)"#)),
             |text_match| {
                 let value = match text_match.group(1).as_ref() {
                     "cero" => 0,
@@ -77,7 +77,7 @@ pub fn rules_numbers() -> RustlingResult<RuleSet<Dimension>> {
         },
         rule! {
             "number (16..19 21..29)",
-            (regex!(r#"(die(c|s)is(é|e)is|diecisiete|dieciocho|diecinueve|veintiun(o|a)|veintidos|veintitr(é|e)s|veinticuatro|veinticinco|veintis(é|e)is|veintisiete|veintiocho|veintinueve)"#)),
+            (regex!(r#"(die(?:c|s)is(?:é|e)is|diecisiete|dieciocho|diecinueve|veintiun(?:o|a)|veintidos|veintitr(?:é|e)s|veinticuatro|veinticinco|veintis(?:é|e)is|veintisiete|veintiocho|veintinueve)"#)),
             |text_match| {
                 let value = match text_match.group(1).as_ref() {
                     "dieciseis" => 16, 
@@ -106,7 +106,7 @@ pub fn rules_numbers() -> RustlingResult<RuleSet<Dimension>> {
         },
         rule! {
             "number 100..1000",
-            (regex!(r#"cien(to)?s?|doscientos|trescientos|cuatrocientos|quinientos|seiscientos|setecientos|ochocientos|novecientos|mil)"#)),
+            (regex!(r#"(cien(?:to)?s?|doscientos|trescientos|cuatrocientos|quinientos|seiscientos|setecientos|ochocientos|novecientos|mil)"#)),
             |text_match| {
                 let value = match text_match.group(1).as_ref() {
                     "cien" => 100,
@@ -295,5 +295,24 @@ pub fn rules_numbers() -> RustlingResult<RuleSet<Dimension>> {
 }
 
 pub fn examples_numbers() -> Vec<::rustling::train::Example<Dimension>> {
-    unimplemented!()
+    let mut v = vec![];
+    example!(v, check_integer(1), "1", "uno", "una");
+    example!(v, check_integer(11), "once");
+    example!(v, check_integer(16), "dieciséis", "dieciseis", "diesiseis", "diez y seis");
+    example!(v, check_integer(21), "veintiuno", "veinte y uno");
+    example!(v, check_integer(23), "veintitrés", "veinte y tres");
+    example!(v, check_integer(70), "setenta");
+    example!(v, check_integer(78), "setenta y ocho");
+    example!(v, check_integer(80), "ochenta");
+    example!(v, check_integer(33), "33", "treinta y tres", "treinta y 3");
+    example!(v, check_float(1.1), "1,1", "1,10", "01,10");
+    example!(v, check_float(0.77), "0,77", ",77");
+    example!(v, check_integer(100000), "100.000", "100000", "100K", "100k");
+    example!(v, check_integer(300), "trescientos");
+    example!(v, check_integer(243), "243");
+    example!(v, check_integer(3000000), "3M", "3000K", "3000000", "3.000.000");
+    example!(v, check_integer(1200000), "1.200.000", "1200000", "1,2M", "1200K", ",0012G");
+    example!(v, check_integer(-1200000), "- 1.200.000", "-1200000", "menos 1.200.000", "-1,2M", "-1200K", "-,0012G");
+    example!(v, check_float(1.5), "1 punto cinco", "una punto cinco", "1,5");
+    v
 }
