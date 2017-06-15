d7957c84ae3cb63f4b85b062e6fec69dcb11bdf4
hdlj <hubert.delajonquiere@snips.net>
2017-06-15 15:28:04 +0200

    number training example in korean

---
+++ training/src/ko.rs
@@ -0,0 +1,43 @@
+use super::*;
+use rustling_ontology_values::check::*;
+
+pub fn examples_numbers(v: &mut Vec<::rustling::train::Example<Dimension>>) {
+    example!(v, check_integer(0), "0", "영", "빵", "공");
+    example!(v, check_integer(1), "1", "일", "하나", "한");
+    example!(v, check_integer(10), "10", "십", "열");
+    example!(v, check_integer(11), "11", "십일", "열하나", "십하나", "열한");
+    example!(v, check_integer(20), "20", "이십", "스물");
+    example!(v, check_integer(35), "35", "삼십오", "서른다섯");
+    example!(v, check_integer(47), "47", "사십칠", "마흔일곱");
+    example!(v, check_integer(52), "52", "오십이", "쉰둘", "쉰두");
+    example!(v, check_integer(69), "69", "육십구", "예순아홉");
+    example!(v, check_integer(71), "71", "칠십일", "일흔하나", "일흔한");
+    example!(v, check_integer(84), "84", "팔십사", "여든넷");
+    example!(v, check_integer(93), "93", "구십삼", "아흔셋");
+    example!(v, check_integer(100), "100", "백");
+    example!(v, check_integer(123), "123", "백이십삼");
+    example!(v, check_integer(579), "579", "오백칠십구");
+    example!(v, check_integer(1000), "1000", "천");
+    example!(v, check_integer(1723), "1723", "천칠백이십삼");
+    example!(v, check_integer(5619), "5619", "오천육백십구");
+    example!(v, check_integer(10000), "10000", "만", "일만");
+    example!(v, check_integer(12345), "12345", "만이천삼백사십오", "일만이천삼백사십오");
+    example!(v, check_integer(58194), "58194", "오만팔천백구십사");
+    example!(v, check_integer(581900), "581900", "오십팔만천구백");
+    example!(v, check_integer(5819014), "5819014", "오백팔십일만구천십사");
+    example!(v, check_integer(58190148), "58190148", "오천팔백십구만백사십팔");
+    example!(v, check_integer(100000000), "100000000", "일억");
+    example!(v, check_integer(274500000000), "274500000000", "이천칠백사십오억");
+    example!(v, check_integer(100000002), "100000002", "일억이");
+    example!(v, check_integer(27350000), "27350000", "이천칠백삼십오만");
+    example!(v, check_integer(3235698120), "3235698120", "삼십이억삼천오백육십구만팔천백이십");
+    example!(v, check_integer(40234985729), "40234985729", "사백이억삼천사백구십팔만오천칠백이십구");
+    example!(v, check_integer(701239801123), "701239801123", "칠천십이억삼천구백팔십만천백이십삼");
+    example!(v, check_float(3.4), "3.4", "삼점사");
+    example!(v, check_float(4123.3), "4123.3", "사천백이십삼점삼");
+    example!(v, check_float(1.23), "일점이삼");
+    example!(v, check_integer(-3), "-3", "마이너스3", "마이너스삼", "마이너스 3", "마이나스3", "마이나스 3");
+    example!(v, check_float(3.0 / 4.0), "3/4", "사분의삼");
+    example!(v, check_ordinal(25), "스물다섯번째", "이십오번째");
+    example!(v, check_ordinal(1), "첫번째", "첫째");
+}
