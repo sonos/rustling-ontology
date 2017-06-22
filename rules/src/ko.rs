a998fa9311a250aa0389097d7117d4381c600da3
hdlj <hubert.delajonquiere@snips.net>
2017-06-22 11:06:21 +0200

    Remove english rule from korean grammar

--- rules/src/ko.rs
+++ rules/src/ko.rs
@@ -313,21 +313,6 @@ pub fn rule_time(b: &mut RuleSetBuilder<Dimension>) -> RustlingResult<()> {
             true
         )
     );
-    b.rule_2("hhmm (military) am|pm",
-        b.reg(r#"((?:1[012]|0?\d))([0-5]\d)"#)?,
-        b.reg(r#"([ap])\.?m?\.?"#)?,
-        |a, b| {
-            let day_period = if b.group(1) == "a" {
-                helpers::hour(0, false)?.span_to(&helpers::hour(12, false)?, false)?
-            } else {
-                helpers::hour(12, false)?.span_to(&helpers::hour(0, false)?, false)?
-            };
-            Ok(helpers::hour_minute(
-                                a.group(1).parse()?,
-                                a.group(2).parse()?, 
-                                true)?.intersect(&day_period)?.form(Form::TimeOfDay(None)))
-        }
-    );
     // From "am|pm <time-of-day>" rules in the original grammar version
     b.rule_2("<time-of-day> am",
         b.reg(r#"오전|아침|새벽"#)?,
