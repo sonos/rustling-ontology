c9ea22b510cdea5a6b49610ad9fd961e2a81ec7a
Hubert De La Jonquiere <hubert.delajonquiere@snips.net>
2018-02-15 18:17:55 +0100

    fix german grammar

--- grammar/de/src/rules.rs
+++ grammar/de/src/rules.rs
@@ -1273,8 +1273,8 @@ pub fn rules_time(b: &mut RuleSetBuilder<Dimension>) -> RustlingResult<()> {
         time_check!(form!(Form::PartOfDay(PartOfDayForm::Afternoon))),
         time_of_day_check_hour!(1, 7, 13, 19),
         |_, tod| {
-            let period = helpers::hour(1, false)?
-                     .span_to(&helpers::hour(12, false)?, true)?;
+            let period = helpers::hour(13, false)?
+                     .span_to(&helpers::hour(19, false)?, true)?;
             Ok(tod.value().intersect(&period)?
                 .form(tod.value().form.clone()))
         }
@@ -1314,9 +1314,9 @@ pub fn rules_time(b: &mut RuleSetBuilder<Dimension>) -> RustlingResult<()> {
     );
 
     b.rule_2("night <time-of-day>",
-        time_of_day_check_hour!(1, 4),
         time_check!(form!(Form::PartOfDay(PartOfDayForm::Night))),
-        |tod, _| {
+        time_of_day_check_hour!(1, 4),
+        |_, tod| {
             let period = helpers::hour(1, false)?
                      .span_to(&helpers::hour(4, false)?, true)?;
             Ok(tod.value().intersect(&period)?
