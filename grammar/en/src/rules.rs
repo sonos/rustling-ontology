2e64ea04f0f5608be4b35e1ff4963600506837ee
Odile-deVismes <odile.de.vismes@snips.ai>
2018-07-24 17:33:32 +0200

    En rules debug rupees

--- grammar/en/src/rules.rs
+++ grammar/en/src/rules.rs
@@ -1565,7 +1565,7 @@ pub fn rules_finance(b: &mut RuleSetBuilder<Dimension>) -> RustlingResult<()> {
                       |_| Ok(MoneyUnitValue { unit: Some("RUB") })
     );
     b.rule_1_terminal("INR",
-                      b.reg(r#"inr|rs(?:. )?|(?:indian )?rupees?"#)?,
+                      b.reg(r#"inr|rs\.?|(?:indian )?rupees?"#)?,
                       |_| Ok(MoneyUnitValue { unit: Some("INR") })
     );
     b.rule_1_terminal("JPY",
