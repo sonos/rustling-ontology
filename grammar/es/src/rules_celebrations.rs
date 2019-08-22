85162e2e23b2d1e5975fc06c7cdac27d9e00e16d
Johanna Simoens <johanna.simoens@snips.ai>
2019-08-22 18:18:12 +0200

    Add prep (por|para) + Datetime, add celebrations.

--- grammar/es/src/rules_celebrations.rs
+++ grammar/es/src/rules_celebrations.rs
@@ -19,5 +19,30 @@ pub fn rules_celebration(b: &mut RuleSetBuilder<Dimension>) -> RustlingResult<()
                       |_| Ok(helpers::month_day(1, 1)?
                        .form(Form::Celebration))
     );
+    b.rule_1_terminal("Father's day",
+                      b.reg(r#"(?:el )?d[íi]a del padre"#)?,
+                      |_| Ok(helpers::month_day(6, 18)?
+                       .form(Form::Celebration))
+    );
+    b.rule_1_terminal("National day",
+                      b.reg(r#"(?:el )?d[íi]a de la hispanidad"#)?,
+                      |_| Ok(helpers::month_day(7, 4)?
+                       .form(Form::Celebration))
+    );
+    b.rule_1_terminal("All saints days",
+                      b.reg(r#"(?:el )?d[íi]a de todos los santos"#)?,
+                      |_| Ok(helpers::month_day(9, 11)?
+                       .form(Form::Celebration))
+    );
+    b.rule_1_terminal("día de la constitucíon",
+                      b.reg(r#"(?:el )?d[íi]a de la constitucíon"#)?,
+                      |_| Ok(helpers::month_day(6, 14)?
+                       .form(Form::Celebration))
+    );
+    b.rule_1_terminal("Women's day",
+                      b.reg(r#"(?:el )?d[íi]a de la mujer"#)?,
+                      |_| Ok(helpers::month_day(8, 26)?
+                       .form(Form::Celebration))
+    );
     Ok(())
 }
