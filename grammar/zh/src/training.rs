4bded446eaa23c3c1f1cbf2f2570567a6ce4d679
Adrien Ball <adrien.ball@snips.ai>
2020-01-23 14:17:17 +0100

    Remove faulty ZH training examples

--- grammar/zh/src/training.rs
+++ grammar/zh/src/training.rs
@@ -103,24 +103,8 @@ pub fn examples_datetime(v: &mut Vec<::rustling::train::Example<Dimension>>) {
 pub fn examples_temperature(v: &mut Vec<::rustling::train::Example<Dimension>>) {
     example!(v, check_temperature(45.0, Some("degree")), "45°", "+45°", "45度");
     example!(v, check_temperature(45.0, Some("degree")), "45°", "45度");
-    example!(v,
-             check_temperature(50.0, Some("fahrenheit")),
-             "50°F",
-             "华氏50°",
-             "華氏50°",
-             "华氏50度",
-             "華氏50度",
-             "50华氏度",
-             "50華氏度");
-    example!(v,
-             check_temperature(23.0, Some("celsius")),
-             "23°C",
-             "摄氏23°",
-             "攝氏23°",
-             "摄氏23度",
-             "攝氏23度",
-             "23摄氏度",
-             "23攝氏度");
+    example!(v, check_temperature(50.0, Some("fahrenheit")), "50°F");
+    example!(v, check_temperature(23.0, Some("celsius")), "23°C");
 }
 
 
